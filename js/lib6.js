e5cbe3a63528d8c2d106e3ec96f6fb722fe72306
ngan <huyhthikimngan110124244@gmail.com>
2026-05-04 16:32:57 +0700

    cap nhat

---
+++ js/lib6.js
@@ -0,0 +1,6 @@
+const productList = [
+    { id: "01", name: "Hoa tulip", price: 20000, image: "../assets/images/tulip.jpg", productLink: "product-detail.html"},
+    { id: "02", name: "Hoa hồng", price: 15000, image: "../assets/images/rose.jpg", productLink: "product-detail.html"},
+    { id: "03", name: "Hoa hướng dương", price: 10000, image: "../assets/images/huongduong.jpg", productLink: "product-detail.html"},
+    { id: "04", name: "Hoa cẩm tú cầu", price: 15000, image: "../assets/images/camtucau.jpg", productLink: "product-detail.html"}
+];
